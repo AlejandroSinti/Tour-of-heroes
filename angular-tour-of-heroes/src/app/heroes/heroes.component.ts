63346b681081fcdfe8bed0f1609df229f3463f47
Alejandro <UniandesLogin@uniandes.edu.co>
2023-08-23 17:25:03 -0500

    Se agrega tabla y cards v2

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 @Component({
